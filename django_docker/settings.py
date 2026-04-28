fb46c8b0d9ee2324804b7b4caa4a8f4a31298187
whitehole17 <cookie69819@gmail.com>
2026-04-28 10:04:39 +0900

    AWS instance 설정 추가

--- django_docker/settings.py
+++ django_docker/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '13.125.120.202',
+    'localhost',
+]
 
 
 # Application definition
